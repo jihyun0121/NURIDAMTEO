b76c6b5bf4fbf0dce56ef7fef122bef7d614190c
jihyun0121 <gheun098@gmail.com>
2025-12-08 18:06:41 +0900

    컬러 시스템 디자인 토큰 추가

---
+++ frontend/src/assets/style/tokens/colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+    gray: {
+        light: { base: "#eeeeee", hover: "#e6e6e6", active: "#cacaca" },
+        normal: { base: "#555555", hover: "#4d4d4d", active: "#444444" },
+        dark: { base: "#333333", hover: "#2d2d2d", active: "#262626" },
+        darker: { base: "#1e1e1e" },
+    },
+    orange: {
+        light: { base: "#fff5ec", hover: "#fff0e3", active: "#ffe0c5" },
+        normal: { base: "#ff9b43", hover: "#e68c3c", active: "#cc7c36" },
+        dark: { base: "#bf7432", hover: "#995d28", active: "#73461e" },
+        darker: { base: "#593617" },
+    },
+    yellow: {
+        light: { base: "#fff8eb", hover: "#fff5e1", active: "#ffeac1" },
+        normal: { base: "#ffbc38", hover: "#e6a932", active: "#cc962d" },
+        dark: { base: "#bf8d2a", hover: "#997122", active: "#735519" },
+        darker: { base: "#594214" },
+    },
+};
