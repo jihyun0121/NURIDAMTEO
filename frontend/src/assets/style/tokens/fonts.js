310b62921bbad443f06364e3a2084ad507f83240
jihyun0121 <gheun098@gmail.com>
2025-12-08 18:07:05 +0900

    폰트 시스템 디자인 토큰 추가

---
+++ frontend/src/assets/style/tokens/fonts.js
@@ -0,0 +1,30 @@
+export const typography = {
+    headline: {
+        xlarge: { size: "44px", weight: 700, line: "56px" },
+        large: { size: "36px", weight: 700, line: "48px" },
+        medium: { size: "28px", weight: 700, line: "36px" },
+        small: { size: "28px", weight: 500, line: "36px" },
+        xsmall: { size: "26px", weight: 800, line: "32px" },
+    },
+
+    title: {
+        xlarge: { size: "24px", weight: 700, line: "32px" },
+        large: { size: "22px", weight: 700, line: "28px" },
+        medium: { size: "20px", weight: 700, line: "26px" },
+        small: { size: "18px", weight: 700, line: "24px" },
+    },
+
+    body: {
+        xlarge: { size: "24px", weight: 500, line: "32px" },
+        large: { size: "22px", weight: 500, line: "28px" },
+        medium: { size: "20px", weight: 500, line: "26px" },
+        small: { size: "18px", weight: 500, line: "24px" },
+    },
+
+    label: {
+        xlarge: { size: "18px", weight: 700, line: "22px" },
+        large: { size: "16px", weight: 500, line: "20px" },
+        medium: { size: "14px", weight: 800, line: "18px" },
+        small: { size: "14px", weight: 700, line: "18px" },
+    },
+};
