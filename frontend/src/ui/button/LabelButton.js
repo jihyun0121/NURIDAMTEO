b45913b0c59175265b575e6666fb84770b88bd61
jihyun0121 <gheun098@gmail.com>
2025-12-09 16:17:58 +0900

    label_Button ui 컴포넌트 추가

---
+++ frontend/src/ui/button/LabelButton.js
@@ -0,0 +1,39 @@
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function NuridamIcon({ content, type = "gray", onClick, disabled = false, ...props }) {
+    let style = {};
+
+    if (type === "gray") {
+        style = {
+            borderColor: colors.gray.normal.base,
+            color: colors.gray.normal.base,
+        };
+    } else if (type === "red") {
+        style = {
+            borderColor: colors.red,
+            color: colors.red,
+        };
+    } else if (type === "green") {
+        style = {
+            borderColor: colors.green,
+            color: colors.green,
+        };
+    } else if (type === "primary") {
+        style = {
+            borderColor: colors.orange.normal.base,
+            color: colors.orange.normal.base,
+        };
+    } else if (type === "fill") {
+        style = {
+            borderColor: colors.orange.normal.base,
+            backgroundColor: colors.orange.normal.base,
+            color: "#FFF",
+        };
+    }
+
+    return (
+        <button className="label-btn" style={{ ...style }} onClick={onClick} disabled={disabled} {...props}>
+            {content}
+        </button>
+    );
+}
