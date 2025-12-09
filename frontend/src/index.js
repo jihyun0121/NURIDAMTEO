65f0c67d9c8daf23ba44e5a657a6fe6b41a4adb7
jihyun0121 <gheun098@gmail.com>
2025-12-09 09:40:40 +0900

    리액트 실행 루트 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/style/font.css";
 
-const root = ReactDOM.createRoot(document.getElementById("roots"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // <React.StrictMode>
     <App />
