34fc6f3a931e279c92a6dc18207a5fbdf55b2d03
jihyun0121 <gheun098@gmail.com>
2025-12-10 08:50:56 +0900

    Auth_Button ui 컴포넌트 css 수정

--- frontend/src/ui/button/AuthButton.js
+++ frontend/src/ui/button/AuthButton.js
@@ -6,22 +6,18 @@ export default function TextButtonS({ content, onClick }) {
     const [state, setState] = useState("default");
 
     const styles = {
-        default: {
-            color: colors.gray.normal.base,
-        },
+        default: {},
         hover: {
-            color: colors.gray.normal.base,
             backgroundColor: colors.orange.light.hover,
         },
         action: {
-            color: colors.white,
-            backgroundColor: colors.orange.normal.base,
+            backgroundColor: colors.orange.light.active,
         },
     };
 
     return (
         <button className="auth-btn" style={styles[state]} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")} onClick={onClick}>
-            <NuridamIcon />
+            <NuridamIcon type="nuri" size="25" />
             {content}
         </button>
     );
