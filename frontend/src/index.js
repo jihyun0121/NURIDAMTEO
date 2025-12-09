98277f8c37025aacad38e4ca043402b06cb5a3cd
jihyun0121 <gheun098@gmail.com>
2025-12-09 16:02:00 +0900

    부트스트랩 제거

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/style/style.css";
 import "./assets/style/font.css";
 import "./assets/style/button.css";
