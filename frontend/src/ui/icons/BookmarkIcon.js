92c3df7ecb46a96f881ed80d29190e87d41548d6
jihyun0121 <gheun098@gmail.com>
2025-12-09 14:33:31 +0900

    Bookmark_icon ui 컴포넌트 타입 분리 및 수정

--- frontend/src/ui/icons/BookmarkIcon.js
+++ frontend/src/ui/icons/BookmarkIcon.js
@@ -1,46 +1,55 @@
-import { useState } from "react";
 import { colors } from "../../assets/style/tokens/colors";
 
 export default function BookmarkIcon({ variant, type = "default", size }) {
-    const [state, setState] = useState("default");
+    let icon = null;
 
-    const icons = {
-        default: (
+    if (type === "default") {
+        icon = (
             <>
-                {type === "fill" && <rect width="44" height="44" rx="22" fill={colors.gray.light.base} />}
                 {variant === "line" ? (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.gray.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 ) : (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.gray.normal.base} stroke={colors.gray.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.gray.normal.base} stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 )}
             </>
-        ),
-        hover: (
+        );
+    } else if (type === "fill") {
+        icon = (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.gray.light.base} />
+                {variant === "line" ? (
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                ) : (
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.gray.normal.base} stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                )}
+            </>
+        );
+    } else if (type === "hover") {
+        icon = (
             <>
                 <rect width="44" height="44" rx="22" fill={colors.orange.light.base} />
                 {variant === "line" ? (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.orange.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 ) : (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 )}
-                FF9B43
             </>
-        ),
-        action: (
+        );
+    } else if (type === "active") {
+        icon = (
             <>
                 <rect width="44" height="44" rx="22" fill={colors.orange.light.active} />
                 {variant === "line" ? (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" stroke={colors.orange.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 ) : (
-                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 13.5686V32L22 26.5098L30 32V13.5686C30 13.1526 29.8314 12.7536 29.5314 12.4594C29.2313 12.1653 28.8243 12 28.4 12H15.6C15.1757 12 14.7687 12.1653 14.4686 12.4594C14.1686 12.7536 14 13.1526 14 13.5686Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 )}
-                FF9B43
             </>
-        ),
-    };
+        );
+    }
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 44 44" fill="none" style={{ cursor: "pointer" }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")}>
-            {icons[state]}
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 44 44" fill="none">
+            {icon}
         </svg>
     );
 }
