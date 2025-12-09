578c4222bb37530288e9d6fa893bf520e020b2ef
jihyun0121 <gheun098@gmail.com>
2025-12-09 12:00:35 +0900

    Menu_icon ui 컴포넌트 추가

---
+++ frontend/src/ui/icons/MenuIcon.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function MenuIcon({ type = "default", size }) {
+    const [state, setState] = useState("default");
+
+    const icons = {
+        default: (
+            <>
+                {type === "fill" && <rect width="44" height="44" rx="22" fill={colors.gray.light.base} />}
+                <path d="M12 14H32M12 22H32M12 30H32" stroke={colors.gray.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        hover: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.base} />
+                <path d="M12 14H32M12 22H32M12 30H32" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        action: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.active} />
+                <path d="M12 14H32M12 22H32M12 30H32" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+    };
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 44 44" fill="none" style={{ cursor: "pointer" }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")}>
+            {icons[state]}
+        </svg>
+    );
+}
