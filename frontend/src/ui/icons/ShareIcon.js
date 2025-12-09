f3cebf60e5e2b7266b579f1eb7d60086b85e125b
jihyun0121 <gheun098@gmail.com>
2025-12-09 11:59:46 +0900

    Share_icon ui 컴포넌트 추가

---
+++ frontend/src/ui/icons/ShareIcon.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function ShareIcon({ type = "default", size }) {
+    const [state, setState] = useState("default");
+
+    const icons = {
+        default: (
+            <>
+                {type === "fill" && <rect width="44" height="44" rx="22" fill={colors.gray.light.base} />}
+                <path d="M24.2222 12H32V19.7778M32 25.0411V30.3333C32 30.7754 31.8244 31.1993 31.5118 31.5118C31.1993 31.8244 30.7754 32 30.3333 32H13.6667C13.2246 32 12.8007 31.8244 12.4882 31.5118C12.1756 31.1993 12 30.7754 12 30.3333V13.6667C12 13.2246 12.1756 12.8007 12.4882 12.4882C12.8007 12.1756 13.2246 12 13.6667 12H18.6667M23 21L31.5 12.5" stroke={colors.gray.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        hover: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.base} />
+                <path d="M24.2222 12H32V19.7778M32 25.0411V30.3333C32 30.7754 31.8244 31.1993 31.5118 31.5118C31.1993 31.8244 30.7754 32 30.3333 32H13.6667C13.2246 32 12.8007 31.8244 12.4882 31.5118C12.1756 31.1993 12 30.7754 12 30.3333V13.6667C12 13.2246 12.1756 12.8007 12.4882 12.4882C12.8007 12.1756 13.2246 12 13.6667 12H18.6667M23 21L31.5 12.5" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        action: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.active} />
+                <path d="M24.2222 12H32V19.7778M32 25.0411V30.3333C32 30.7754 31.8244 31.1993 31.5118 31.5118C31.1993 31.8244 30.7754 32 30.3333 32H13.6667C13.2246 32 12.8007 31.8244 12.4882 31.5118C12.1756 31.1993 12 30.7754 12 30.3333V13.6667C12 13.2246 12.1756 12.8007 12.4882 12.4882C12.8007 12.1756 13.2246 12 13.6667 12H18.6667M23 21L31.5 12.5" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+    };
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 44 44" fill="none" style={{ cursor: "pointer" }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")}>
+            {icons[state]}
+        </svg>
+    );
+}
