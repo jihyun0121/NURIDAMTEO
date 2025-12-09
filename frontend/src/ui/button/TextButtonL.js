9f900f5c06a7359dcd92d8e22dd16197a556fb7a
jihyun0121 <gheun098@gmail.com>
2025-12-09 17:57:10 +0900

    Text_Button_s, Text_Button_l ui 컴포넌트 추가

---
+++ frontend/src/ui/button/TextButtonL.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function TextButtonS({ children, content, onClick }) {
+    const [state, setState] = useState("default");
+
+    const styles = {
+        default: {
+            color: colors.gray.normal.base,
+        },
+        hover: {
+            color: colors.gray.normal.base,
+            backgroundColor: colors.orange.light.hover,
+        },
+        action: {
+            color: colors.white,
+            backgroundColor: colors.orange.normal.base,
+        },
+    };
+
+    return (
+        <button className="text-btn-l" style={styles[state]} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")} onClick={onClick}>
+            {children}
+            {content}
+        </button>
+    );
+}
