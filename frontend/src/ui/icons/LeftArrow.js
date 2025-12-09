7df33c62cef996ea83ed4b7cd1cbf342337583eb
jihyun0121 <gheun098@gmail.com>
2025-12-09 10:03:50 +0900

    Leftarrow_icon ui 컴포넌트 추가

---
+++ frontend/src/ui/icons/LeftArrow.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function LeftArrow({ type = "default" }) {
+    const [state, setState] = useState("default");
+
+    const icons = {
+        default: (
+            <>
+                {type === "fill" && <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#EEEEEE" />}
+                <path d="M24 14L16 22L24 30" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        hover: (
+            <>
+                <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#FFF5EC" />
+                <path d="M24 14L16 22L24 30" stroke="#FF9B43" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+        action: (
+            <>
+                <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#FFE0C5" />
+                <path d="M24 14L16 22L24 30" stroke="#FF9B43" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </>
+        ),
+    };
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none" style={{ cursor: "pointer" }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")}>
+            {icons[state]}
+        </svg>
+    );
+}
