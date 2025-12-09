708c18d97f929b70c5e8d3b0522cc44722c9d531
jihyun0121 <gheun098@gmail.com>
2025-12-09 18:03:33 +0900

    Auth_button ui 컴포넌트 추가

---
+++ frontend/src/ui/button/AuthButton.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { colors } from "../../assets/style/tokens/colors";
+import NuridamIcon from "../icons/NuridamIcon";
+
+export default function TextButtonS({ content, onClick }) {
+    const [state, setState] = useState("default");
+
+    const styles = {
+        default: {
+            color: colors.gray.normal.base,
+        },
+        hover: {
+            color: colors.gray.normal.base,
+            backgroundColor: colors.orange.light.hover,
+        },
+        action: {
+            color: colors.white,
+            backgroundColor: colors.orange.normal.base,
+        },
+    };
+
+    return (
+        <button className="auth-btn" style={styles[state]} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")} onClick={onClick}>
+            <NuridamIcon />
+            {content}
+        </button>
+    );
+}
