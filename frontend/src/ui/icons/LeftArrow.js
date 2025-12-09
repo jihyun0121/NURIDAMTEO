0b7672bf80454ad9aa10c8140fc25f80bd4a4f23
jihyun0121 <gheun098@gmail.com>
2025-12-09 10:10:17 +0900

    Rightarrow_icon ui 컴포넌트 추가

--- frontend/src/ui/icons/LeftArrow.js
+++ frontend/src/ui/icons/LeftArrow.js
@@ -7,19 +7,19 @@ export default function LeftArrow({ type = "default" }) {
         default: (
             <>
                 {type === "fill" && <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#EEEEEE" />}
-                <path d="M24 14L16 22L24 30" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M24 14L16 22L24 30" stroke="#555555" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </>
         ),
         hover: (
             <>
                 <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#FFF5EC" />
-                <path d="M24 14L16 22L24 30" stroke="#FF9B43" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M24 14L16 22L24 30" stroke="#FF9B43" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </>
         ),
         action: (
             <>
                 <rect y="44" width="44" height="44" rx="22" transform="rotate(-90 0 44)" fill="#FFE0C5" />
-                <path d="M24 14L16 22L24 30" stroke="#FF9B43" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M24 14L16 22L24 30" stroke="#FF9B43" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </>
         ),
     };
