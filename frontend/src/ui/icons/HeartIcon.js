86093a7c74e8b67bc87fa62361a9aca3966569eb
jihyun0121 <gheun098@gmail.com>
2025-12-09 12:00:04 +0900

    Heart_icon ui 컴포넌트 추가

---
+++ frontend/src/ui/icons/HeartIcon.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function HeartIcon({ variant, type = "default", size }) {
+    const [state, setState] = useState("default");
+
+    const icons = {
+        default: (
+            <>
+                {type === "fill" && <rect width="44" height="44" rx="22" fill={colors.gray.light.base} />}
+                {variant === "line" ? (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" fill="none" stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                ) : (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" fill={colors.gray.normal.base} stroke={colors.gray.normal.base} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                )}
+            </>
+        ),
+        hover: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.base} />
+                {variant === "line" ? (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                ) : (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                )}
+            </>
+        ),
+        action: (
+            <>
+                <rect width="44" height="44" rx="22" fill={colors.orange.light.active} />
+                {variant === "line" ? (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                ) : (
+                    <path d="M16.5 12C13.4629 12 11 14.4853 11 17.5516C11 20.0268 11.9625 25.9014 21.4368 31.84C21.6067 31.9447 21.8015 32 22 32C22.1985 32 22.3933 31.9447 22.5632 31.84C32.0386 25.9014 33 20.0268 33 17.5516C33 14.4853 30.5371 12 27.5 12C24.4629 12 22 15.3646 22 15.3646C22 15.3646 19.5371 12 16.5 12Z" fill={colors.orange.normal.base} stroke={colors.orange.normal.base} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                )}
+            </>
+        ),
+    };
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 44 44" fill="none" style={{ cursor: "pointer" }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")}>
+            {icons[state]}
+        </svg>
+    );
+}
