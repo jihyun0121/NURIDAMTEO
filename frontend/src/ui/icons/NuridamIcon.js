61fcf103fe3ee3bd1f39372cd02e12a945f08145
jihyun0121 <gheun098@gmail.com>
2025-12-09 15:13:51 +0900

    Nuridam_icon ui 컴포넌트 추가

---
+++ frontend/src/ui/icons/NuridamIcon.js
@@ -0,0 +1,23 @@
+import { ReactComponent as UserIcon } from "../../assets/image/User.svg";
+import { ReactComponent as Guruem } from "../../assets/image/Guruem.svg";
+import { ReactComponent as Sodam } from "../../assets/image/Sodam.svg";
+import { ReactComponent as Nuri } from "../../assets/image/Nuri.svg";
+
+const ICON_MAP = {
+    user: UserIcon,
+    guruem: Guruem,
+    sodam: Sodam,
+    nuri: Nuri,
+};
+
+export default function NuridamIcon({ type = "user", size = 44 }) {
+    const IconComponent = ICON_MAP[type.toLowerCase()];
+
+    if (!IconComponent) return null;
+
+    return (
+        <div style={{ width: size, height: size }}>
+            <IconComponent width={size} height={size} />
+        </div>
+    );
+}
