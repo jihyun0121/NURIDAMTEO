415d20c0af8dff68afce8190d6eb3026a969fd2d
jihyun0121 <gheun098@gmail.com>
2025-12-10 11:52:28 +0900

    Profile ui 컴포넌트 추가

---
+++ frontend/src/ui/Profile.js
@@ -0,0 +1,23 @@
+import LevelLabel from "./LevelLabel";
+import { ReactComponent as UserProfile } from "../assets/image/profile/User_Icon.svg";
+
+export default function AuthButton({ userId }) {
+    const email = "username@naver.com";
+    const name = "User Name";
+    const level = 1;
+    const profile = <UserProfile />;
+    const type = "guruem";
+
+    return (
+        <div className="profile">
+            <div className="profile-img">{profile}</div>
+            <div className="profile-text">
+                <div className="profile-name">
+                    <LevelLabel level={level} type={type} />
+                    {name}님
+                </div>
+                <div className="profile-email">{email}</div>
+            </div>
+        </div>
+    );
+}
