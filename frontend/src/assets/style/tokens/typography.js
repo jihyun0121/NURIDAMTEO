42ce764f99d101cfc2410210c4007deff83f397e
jihyun0121 <gheun098@gmail.com>
2025-12-11 15:46:55 +0900

    폰트 시스템 두께 변경

--- frontend/src/assets/style/tokens/typography.js
+++ frontend/src/assets/style/tokens/typography.js
@@ -2,8 +2,8 @@ export const typography = {
     headline: {
         xlarge: { size: "44px", weight: 700, line: "56px" },
         large: { size: "36px", weight: 700, line: "48px" },
-        medium: { size: "28px", weight: 700, line: "36px" },
-        small: { size: "28px", weight: 500, line: "36px" },
+        medium: { size: "28px", weight: 800, line: "36px" },
+        small: { size: "28px", weight: 700, line: "36px" },
         xsmall: { size: "26px", weight: 800, line: "32px" },
     },
     title: {
