744122e3d244854b1c190794160ff4024075102b
jihyun0121 <gheun098@gmail.com>
2025-12-11 11:51:53 +0900

    Search_Bar ui 컴포넌트 추가

---
+++ frontend/src/ui/input/SearchBar.js
@@ -0,0 +1,18 @@
+import SearchIcon from "../icons/SearchIcon";
+import SearchDropdqon from "../input/SearchDropdown";
+
+export default function SearchBar({ type = "short", onClick }) {
+    const styles = {
+        width: type === "long" ? "1520px" : "1261px",
+    };
+
+    return (
+        <div className="search-container" style={styles} onClick={onClick}>
+            <SearchDropdqon />
+            <div className="search-content" style={styles} onClick={onClick}>
+                <input className="search-input" placeholder="검색어를 입력해주세요"></input>
+                <SearchIcon size={44} />
+            </div>
+        </div>
+    );
+}
