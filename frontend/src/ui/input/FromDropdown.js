dac410a059c4c063818c5349e8326d42fcc469f5
jihyun0121 <gheun098@gmail.com>
2025-12-11 11:23:37 +0900

    Form_Dropdown ui 컴포넌트 추가

---
+++ frontend/src/ui/input/FromDropdown.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import ArrowIcon from "../icons/ArrowIcon";
+
+export default function Dropdown({ optionData = [] }) {
+    const [currentValue, setCurrentValue] = useState("Text");
+    const [showOptions, setShowOptions] = useState(false);
+
+    const handleSelect = (value, e) => {
+        e.stopPropagation();
+        setCurrentValue(value);
+        setShowOptions(false);
+    };
+
+    const toggleDropdown = (e) => {
+        e.stopPropagation();
+        setShowOptions((prev) => !prev);
+    };
+
+    return (
+        <div className="dropdown-box" onClick={toggleDropdown}>
+            <div className="dropdown-label">{currentValue}</div>
+            <ArrowIcon direction={showOptions ? "up" : "down"} size={44} color="inherit" />
+
+            {showOptions && (
+                <div className="dropdown-option">
+                    {/* {optionData.map((data) => (
+                        <div className="dropdown-text" key={data.key} onClick={() => handleSelect(data.value)}>
+                            {data.value}
+                        </div>
+                    ))} */}
+
+                    <div className="dropdown-text" onClick={(e) => handleSelect("Text1", e)}>
+                        Text1
+                    </div>
+                    <div className="dropdown-text" onClick={(e) => handleSelect("Text2", e)}>
+                        Text2
+                    </div>
+                    <div className="dropdown-text" onClick={(e) => handleSelect("Text3", e)}>
+                        Text3
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
