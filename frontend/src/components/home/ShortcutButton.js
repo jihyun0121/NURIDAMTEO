260f3d1c96d03b7c6a674ec9c8656001ddd24b0c
jihyun0121 <gheun098@gmail.com>
2025-12-12 13:56:06 +0900

    홈 화면 바로가기 버튼 컴포넌트 추가

---
+++ frontend/src/components/home/ShortcutButton.js
@@ -0,0 +1,12 @@
+import { colors } from "../../assets/style/tokens/colors";
+
+export default function ShortcutButton() {
+    return (
+        <>
+            <div style={{ display: "flex" }}>
+                <button className="shortcut-btn-left"></button>
+                <button className="shortcut-btn-right"></button>
+            </div>
+        </>
+    );
+}
