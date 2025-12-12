31f314a8745bbe2ff7054dc9c241e3cdcbe10fe9
jihyun0121 <gheun098@gmail.com>
2025-12-12 13:45:41 +0900

    헤더 컴포넌트 추가

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,33 @@
+import { useLocation } from "react-router-dom";
+import Logo from "../ui/Logo";
+import SearchIcon from "../ui/icons/SearchIcon";
+import BellIcon from "../ui/icons/BellIcon";
+import Icon from "../ui/icons/TextIcon";
+import HeaderButton from "../components/HeaderButton";
+
+export default function Header() {
+    const location = useLocation();
+    const pathname = location.pathname;
+
+    const activeTab = pathname.startsWith("/participate") ? "join" : pathname.startsWith("/proposal") ? "prop" : pathname.startsWith("/nurisodam") ? "nuri" : "none";
+
+    return (
+        <div className="header-container">
+            <div style={{ cursor: "pointer" }} onClick={() => (window.location.href = "/")}>
+                <Logo size="m" />
+            </div>
+
+            <div className="header-menu">
+                <HeaderButton content={"참여하기"} active={activeTab === "join"} onClick={() => (window.location.href = "/participate")} type="white" />
+                <HeaderButton content={"시민제안"} active={activeTab === "prop"} onClick={() => (window.location.href = "/proposal")} type="white" />
+                <HeaderButton content={"누리소담"} active={activeTab === "nuri"} onClick={() => (window.location.href = "/nurisodam")} type="white" />
+            </div>
+
+            <div className="header-icons">
+                <SearchIcon size={44} type="fill" />
+                <BellIcon size={44} type="fill" />
+                <Icon size={44} type="fill" />
+            </div>
+        </div>
+    );
+}
