aee2f1e29e76b471c8cadd4cf0a49f3df4be76ae
jihyun0121 <gheun098@gmail.com>
2025-12-12 16:03:15 +0900

    제안페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
+import ParticipatePage from "./pages/ParticipatePage";
+import ProposalPage from "./pages/ProposalPage";
+import NurisodamPage from "./pages/NurisodamPage";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/participate" element={<ParticipatePage />} />
+                <Route path="/proposal" element={<ProposalPage />} />
+                <Route path="/nurisodam" element={<NurisodamPage />} />
             </Routes>
         </Router>
     );
