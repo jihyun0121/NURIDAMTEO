90b1f52a6d5de5540a72ccbefcac26b4f6915285
jihyun0121 <gheun098@gmail.com>
2025-12-15 14:14:24 +0900

    질문 dto 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/dtos/QuestionDTO.java
@@ -0,0 +1,29 @@
+package com.nuridamteo.backend.dtos;
+
+import com.fasterxml.jackson.annotation.*;
+import com.nuridamteo.backend.enums.QuestionType;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionDTO {
+    @JsonProperty("question_id")
+    private Long questionId;
+
+    @JsonProperty("survey_id")
+    private Long survey;
+
+    @JsonProperty("question_content")
+    private String questionContent;
+
+    @JsonProperty("question_type")
+    @Builder.Default
+    private QuestionType questionType = QuestionType.CHECK;
+
+    @JsonProperty("question_order")
+    private Long questionOrder;
+}
