ca9f7fde28d1c720d998b4a749931b4e153eaafd
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:30:42 +0900

    카테고리 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Category.java
@@ -0,0 +1,30 @@
+package com.nuridamteo.backend.entities;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    @JsonProperty("category_id")
+    private Long categoryId;
+
+    @Column(name = "category_name", nullable = false, length = 100)
+    @JsonProperty("category_name")
+    private String categoryName;
+
+    @Column(name = "category_type", nullable = false, length = 50)
+    @JsonProperty("category_type")
+    private String categoryType;
+}
