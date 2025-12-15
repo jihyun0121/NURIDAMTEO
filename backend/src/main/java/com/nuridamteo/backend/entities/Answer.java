21d87b1298e7cc0b292b2f8cfd35febc247a1e42
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:33:06 +0900

    응답 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Answer.java
@@ -0,0 +1,44 @@
+package com.nuridamteo.backend.entities;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
+    @JsonProperty("answer_id")
+    private Long answerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participation_id", nullable = false, unique = true)
+    @JsonProperty("participation_id")
+    @JsonIgnore
+    private Participation participation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false, unique = true)
+    @JsonProperty("question_id")
+    @JsonIgnore
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", unique = true)
+    @JsonProperty("option_id")
+    @JsonIgnore
+    private Options option;
+
+    @Column(name = "answer_text")
+    @JsonProperty("answer_text")
+    private String answerText;
+}
