62b86261112713dc3923a75824abc231f225090e
jihyun0121 <gheun098@gmail.com>
2025-12-15 14:14:07 +0900

    알림 dto 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/dtos/NotificationDTO.java
@@ -0,0 +1,36 @@
+package com.nuridamteo.backend.dtos;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NotificationDTO {
+    @JsonProperty("notification_id")
+    private Long notificationId;
+
+    @JsonProperty("user_id")
+    private Long user;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("is_read")
+    @Builder.Default
+    private Boolean isRead = false;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("read_at")
+    private LocalDateTime readAt;
+}
