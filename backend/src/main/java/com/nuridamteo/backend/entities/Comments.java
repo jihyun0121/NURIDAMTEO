e118e528f07407707340d9cb23f1b7b202023ae1
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:33:29 +0900

    답글 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Comments.java
@@ -0,0 +1,50 @@
+package com.nuridamteo.backend.entities;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    @JsonProperty("comment_id")
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participation_id")
+    @JsonProperty("participation_id")
+    @JsonIgnore
+    private Participation participation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "suggestion_id")
+    @JsonProperty("suggestion_id")
+    @JsonIgnore
+    private Suggestion suggestion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonProperty("user_id")
+    @JsonIgnore
+    private Users user;
+
+    @Column(name = "content", nullable = false)
+    @JsonProperty("content")
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+}
