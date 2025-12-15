52ced092326d50850a406365becb15fe69bb61d6
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:34:44 +0900

    출석 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Attendance.java
@@ -0,0 +1,34 @@
+package com.nuridamteo.backend.entities;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id")
+    @JsonProperty("attendance_id")
+    private Long attendanceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JsonProperty("user_id")
+    @JsonIgnore
+    private Users user;
+
+    @Column(name = "attendance_date", nullable = false)
+    @JsonProperty("attendance_date")
+    private LocalDateTime attendanceDate;
+}
