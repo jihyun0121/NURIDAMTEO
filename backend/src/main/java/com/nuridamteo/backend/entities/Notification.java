74f96b4d2886f9f2a80f677eedb11dea6f90e59b
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:34:52 +0900

    알림 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Notification.java
@@ -0,0 +1,51 @@
+package com.nuridamteo.backend.entities;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    @JsonProperty("notification_id")
+    private Long notificationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonProperty("user_id")
+    @JsonIgnore
+    private Users user;
+
+    @Column(name = "title", nullable = false)
+    @JsonProperty("title")
+    private String title;
+
+    @Column(name = "message", nullable = false, length = 200)
+    @JsonProperty("message")
+    private String message;
+
+    @Column(name = "is_read", nullable = false)
+    @JsonProperty("is_read")
+    @Builder.Default
+    private Boolean isRead = false;
+
+    @Column(name = "created_at", nullable = false)
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "read_at", nullable = false)
+    @JsonProperty("read_at")
+    private LocalDateTime readAt;
+}
