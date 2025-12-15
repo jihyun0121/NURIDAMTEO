ea12fe55b6657ccc5d16b23e81e23a8d0d0dadaa
jihyun0121 <gheun098@gmail.com>
2025-12-15 15:57:19 +0900

    사용자 개인정보 테이블 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Profile.java
@@ -0,0 +1,42 @@
+package com.nuridamteo.backend.entities;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "profile")
+public class Profile {
+    @Id
+    @Column(name = "profile_id")
+    private Long userId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 20)
+    private String gender;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    @Column(nullable = false, length = 50)
+    private String residence;
+
+    @Column(name = "postal_code", nullable = false, length = 20)
+    @JsonProperty("postal_code")
+    private String postalCode;
+}
