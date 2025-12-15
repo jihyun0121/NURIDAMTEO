6f02c59213bba7905da30d28c19ccc577eff8728
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:33:20 +0900

    공감(좋아요) 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Likes.java
@@ -0,0 +1,40 @@
+package com.nuridamteo.backend.entities;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    @JsonProperty("like_id")
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "suggestions_id", nullable = false, unique = true)
+    @JsonProperty("suggestions_id")
+    @JsonIgnore
+    private Suggestion suggestions;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    @JsonProperty("user_id")
+    @JsonIgnore
+    private Users user;
+
+    @Column(name = "created_at", nullable = false)
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+}
