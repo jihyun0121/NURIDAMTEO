67eaa8789fc811d3924b7f4e108f0666ee2fc340
jihyun0121 <gheun098@gmail.com>
2025-12-15 13:31:44 +0900

    질문 관련 엔티티 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Options.java
@@ -0,0 +1,36 @@
+package com.nuridamteo.backend.entities;
+
+import com.fasterxml.jackson.annotation.*;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Options {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_id")
+    @JsonProperty("option_id")
+    private Long optionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    @JsonProperty("question_id")
+    @JsonIgnore
+    private Question question;
+
+    @Column(name = "option_content", nullable = false, length = 100)
+    @JsonProperty("option_content")
+    private String optionContent;
+
+    @Column(name = "option_order")
+    @JsonProperty("option_order")
+    private Long optionOrder;
+}
---
+++ backend/src/main/java/com/nuridamteo/backend/entities/Question.java
@@ -0,0 +1,43 @@
+package com.nuridamteo.backend.entities;
+
+import com.fasterxml.jackson.annotation.*;
+import com.nuridamteo.backend.enums.QuestionType;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "level")
+
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    @JsonProperty("question_id")
+    private Long questionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_id", nullable = false)
+    @JsonProperty("survey_id")
+    @JsonIgnore
+    private Survey survey;
+
+    @Column(name = "question_content", nullable = false)
+    @JsonProperty("question_content")
+    private String questionContent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "question_type", length = 10)
+    @JsonProperty("question_type")
+    @Builder.Default
+    private QuestionType questionType = QuestionType.CHECK;
+
+    @Column(name = "question_order")
+    @JsonProperty("question_order")
+    private Long questionOrder;
+}
