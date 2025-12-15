613303dcb3e8e6b94421f0a631861b104beafc03
jihyun0121 <gheun098@gmail.com>
2025-12-15 14:14:01 +0900

    공지 dto 추가

---
+++ backend/src/main/java/com/nuridamteo/backend/dtos/NoticeDTO.java
@@ -0,0 +1,33 @@
+package com.nuridamteo.backend.dtos;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeDTO {
+    @JsonProperty("notice_id")
+    private Long noticeId;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("is_pinned")
+    @Builder.Default
+    private Boolean isPinned = false;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+}
