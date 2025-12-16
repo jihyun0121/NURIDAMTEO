825fbf1ebe0d1a472e1f46ba4bbec472581a2f77
unanseol <d2310@e-mirim.ks.kr>
2025-12-16 17:32:21 +0900

    로그인 박스 생성

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "../components/Header";
+import Logo from "../ui/Logo";
+import TextInputBox from "../ui/input/TextInputBox";
+import TextButtonS from "../ui/button/TextButtonS";
 
 export default function LoginPage() {
     return (
         <>
             <div className="login-container">
-            <Header />
-            
+                <Header />
+                <div className="Login-box">
+                    <Logo />
+                    <h1> 로그인 </h1>
+                    <TextInputBox content="아이디" />
+                    <TextInputBox content="비밀번호" />
+
+                    <TextButtonS content="로그인" type="Fill" />
+                    <TextButtonS content="회원가입" type="None" />
+                </div>
             </div>
         </>
     );
