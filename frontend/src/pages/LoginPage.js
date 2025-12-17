66349cacdf44f419ec844221e3c3914ff46fb92e
unanseol <d2310@e-mirim.hs.kr>
2025-12-17 17:57:52 +0900

    로그인 페이지 로그인 옵션 버튼 추가 및 기능 수정

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,27 +3,39 @@ import Header from "../components/Header";
 import Logo from "../ui/Logo";
 import TextInputBox from "../ui/input/TextInputBox";
 import TextButtonS from "../ui/button/TextButtonS";
+import TextButtonL from "../ui/button/TextButtonL";
 
 export default function LoginPage() {
     return (
-        <>
-            <div className="login-container">
-                <Header />
+        <div className="login-container">
+            <Header />
 
-                <div className="login-box">
-                    <Logo size="l" />
+            <div className="login-box">
+                <Logo size="l" />
 
-                    <div className="login-card">
-                        <div className="login-title">로그인</div>
-
-                        <TextInputBox content="아이디" />
-                        <TextInputBox content="비밀번호" />
-
-                        <TextButtonS content="로그인" type="Fill" />
-                        <TextButtonS content="회원가입" type="None" />
+                <div className="login-buttons">
+                    <TextInputBox
+                        content="아이디"
+                        type="long"
+                        placeholder="아이디"
+                    />
+                    <TextInputBox
+                        content="비밀번호"
+                        type="long"
+                        placeholder="비밀번호"
+                        inputType="password"
+                    />
+                    <div className="login-options">
+                        <span className="login-option-text">아이디 찾기</span>
+                        <span className="login-option-text">비밀번호 찾기</span>
                     </div>
                 </div>
+
+                <div className="login-card">
+                    <TextButtonL content="로그인" type="Fill" />
+                    <TextButtonL content="회원가입" type="None" />
+                </div>
             </div>
-        </>
+        </div>
     );
 }
