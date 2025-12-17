29aa1e0d4ee5bd20b392bf4bd0c743db9f37c561
unanseol <d2310@e-mirim.hs.kr>
2025-12-17 17:32:58 +0900

    placeholder 파라미터 추가

--- frontend/src/ui/input/TextInputBox.js
+++ frontend/src/ui/input/TextInputBox.js
@@ -1,11 +1,16 @@
-export default function TextInputBox({ children, type = "short", onClick }) {
+export default function TextInputBox({
+    children,
+    type = "short",
+    onClick,
+    placeholder,
+}) {
     const styles = {
         width: type === "short" ? "19.75rem" : "40.5rem",
     };
 
     return (
         <div className="text-input-box" style={styles} onClick={onClick}>
-            <input className="text-input" placeholder="Text"></input>
+            <input className="text-input" placeholder={placeholder}></input>
             {children}
         </div>
     );
