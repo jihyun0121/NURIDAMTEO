8aa81186aef21ccad2f27802f41048c4f4237fc2
jihyun0121 <gheun098@gmail.com>
2025-12-17 15:26:39 +0900

    @Repository 어노테이션 추가

--- backend/src/main/java/com/nuridamteo/backend/repositories/AttendanceRepository.java
+++ backend/src/main/java/com/nuridamteo/backend/repositories/AttendanceRepository.java
@@ -4,10 +4,12 @@ import java.time.*;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
 import com.nuridamteo.backend.entities.Attendance;
 import com.nuridamteo.backend.entities.Users;
 
+@Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Optional<Attendance> findByUserAndAttendanceDateBetween(Users user, LocalDateTime start, LocalDateTime end);
 
--- backend/src/main/java/com/nuridamteo/backend/repositories/SurveyRepository.java
+++ backend/src/main/java/com/nuridamteo/backend/repositories/SurveyRepository.java
@@ -3,10 +3,12 @@ package com.nuridamteo.backend.repositories;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
 import com.nuridamteo.backend.entities.*;
 import com.nuridamteo.backend.enums.SurveyType;
 
+@Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     List<Survey> findAllByOrderBySurveyIdDesc();
 
