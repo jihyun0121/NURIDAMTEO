bcaf4f05e55f0b4524331d9ce5bf70aab8ee192b
jihyun0121 <gheun098@gmail.com>
2025-12-19 15:22:36 +0900

    누리소담 화면 공지사항 목록 컴포넌트 추가

---
+++ frontend/src/components/nurisodam/NoticeList.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { NoticeAPI } from "../../api/api";
+import MegaphoneIcon from "../../ui/icons/MegaphoneIcon";
+import EyeIcon from "../../ui/icons/EyeIcon";
+import Pagination from "../Pagination";
+
+const PAGE_SIZE = 3;
+
+export default function NoticeList() {
+    const [notice, setNotice] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        async function loadNotice() {
+            try {
+                const res = await NoticeAPI.getNotice(0);
+                setNotice(res.data || []);
+            } catch (err) {
+                console.log("뉴스 로딩 실패", err);
+            }
+        }
+
+        loadNotice();
+    }, []);
+
+    const totalPages = Math.ceil(notice.length / PAGE_SIZE);
+
+    const startIndex = (currentPage - 1) * PAGE_SIZE;
+    const currentNotices = notice.slice(startIndex, startIndex + PAGE_SIZE);
+
+    return (
+        <div className="nurisodam-list-container">
+            {currentNotices.map((notice) => (
+                <div key={notice.notice_id} className="nurisodam-lists" style={{ cursor: "pointer" }} onClick={() => (window.location.href = `/nurisodam/notice/${notice.notice_id}`)}>
+                    <MegaphoneIcon size="44" type={notice?.is_pinned ? "hover" : "default"} />
+                    <span className="nurisodam-list-text">{notice?.title ?? "제목 없음"}</span>
+
+                    <div className="nurisodam-list-view">
+                        <EyeIcon size="44" />
+                        {notice?.view_count ?? 0}
+                    </div>
+                </div>
+            ))}
+            <Pagination currentPage={currentPage} totalPages={totalPages} onChange={setCurrentPage} />
+        </div>
+    );
+}
