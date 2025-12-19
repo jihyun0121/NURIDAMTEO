77f74d0c1055ac3f50b93fdd2ba581732f42245b
jihyun0121 <gheun098@gmail.com>
2025-12-19 10:42:45 +0900

    TextInputBox readOnly 파라미터 및 로직 추가

--- frontend/src/ui/input/TextInputBox.js
+++ frontend/src/ui/input/TextInputBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function TextInputBox({ children, type = "short", onClick, onChange, value, placeholder = "Text", inputType = "text" }) {
+export default function TextInputBox({ children, type = "short", onClick, onChange, value, placeholder = "Text", inputType = "text", readOnly }) {
     let width;
 
     if (type === "short") width = "19.75rem";
@@ -13,7 +13,7 @@ export default function TextInputBox({ children, type = "short", onClick, onChan
 
     return (
         <div className="text-input-box" style={styles}>
-            <input className="text-input" onClick={onClick} placeholder={placeholder} type={inputType} value={value} onChange={onChange} />
+            <input className="text-input" onClick={onClick} placeholder={placeholder} type={inputType} value={value} onChange={onChange} readOnly={readOnly} style={{ cursor: `${readOnly && "default"}` }} />
             {children}
         </div>
     );
