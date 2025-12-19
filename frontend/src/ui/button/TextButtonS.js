6496462968b541327be046fea96b3db301682c18
jihyun0121 <gheun098@gmail.com>
2025-12-19 17:30:53 +0900

    TextBurronS ui 컴포넌트 yellow 타입 추가

--- frontend/src/ui/button/TextButtonS.js
+++ frontend/src/ui/button/TextButtonS.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import { colors } from "../../assets/style/tokens/colors";
 
-export default function TextButtonS({ children, content, onClick, type = "default" }) {
+export default function TextButtonS({ children, content, onClick, type = "default", style }) {
     const [state, setState] = useState("default");
 
     const styles = {
         default: {
             color: type === "default" ? colors.gray.normal.base : colors.white,
-            backgroundColor: type === "default" ? colors.white : colors.orange.normal.base,
+            backgroundColor: type === "default" ? colors.white : type === "yellow" ? colors.yellow.normal.base : colors.orange.normal.base,
         },
         hover: {
             color: type === "default" ? colors.gray.normal.base : colors.white,
-            backgroundColor: type === "default" ? colors.orange.light.hover : colors.orange.normal.hover,
+            backgroundColor: type === "default" ? colors.orange.light.hover : type === "yellow" ? colors.yellow.normal.hover : colors.orange.normal.hover,
         },
         action: {
             color: colors.white,
-            backgroundColor: type === "default" ? colors.orange.normal.base : colors.orange.normal.active,
+            backgroundColor: type === "default" ? colors.orange.normal.base : type === "yellow" ? colors.yellow.normal.active : colors.orange.normal.active,
         },
     };
 
     return (
-        <button className="text-btn-s" style={styles[state]} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")} onClick={onClick}>
+        <button className="text-btn-s" style={{ ...styles[state], ...style }} onMouseEnter={() => setState("hover")} onMouseLeave={() => setState("default")} onMouseDown={() => setState("action")} onMouseUp={() => setState("hover")} onClick={onClick}>
             {children}
             {content}
         </button>
