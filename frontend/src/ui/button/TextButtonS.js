415b5461fab096cc3664a79c120ab12552917c00
unanseol <d2310@e-mirim.hs.kr>
2025-12-19 16:10:56 +0900

    텍스트버튼 폰트스타일 수정

--- frontend/src/ui/button/TextButtonS.js
+++ frontend/src/ui/button/TextButtonS.js
@@ -7,14 +7,20 @@ export default function TextButtonS({ children, content, onClick }) {
     const styles = {
         default: {
             color: colors.gray.normal.base,
+            fontstyle: { color: "var(--Gray-White, #FFF)" },
+            fontFaceSize: "var(--Font-Tittle-xlarge)",
         },
         hover: {
             color: colors.gray.normal.base,
             backgroundColor: colors.orange.light.hover,
+            fontstyle: { color: "var(--Gray-Normal, #555)" },
+            fontFaceSize: "var(--Font-Tittle-xlarge)",
         },
         action: {
             color: colors.white,
             backgroundColor: colors.orange.normal.base,
+            fontstyle: { color: "var(--Gray-Normal, #555)" },
+            fontFaceSize: "var(--Font-Tittle-xlarge)",
         },
     };
 
