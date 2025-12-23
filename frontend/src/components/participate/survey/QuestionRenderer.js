83bab1bcc31949983ba4fe519fb631071f68f507
jihyun0121 <gheun098@gmail.com>
2025-12-23 17:47:10 +0900

    참여하기 세부 화면 질문 컴포넌트 추가

---
+++ frontend/src/components/participate/survey/QuestionRenderer.js
@@ -0,0 +1,16 @@
+import LabelButton from "../../../ui/button/LabelButton";
+import OptionRenderer from "./OptionRenderer";
+
+export default function QuestionRenderer({ question, answers, onChange }) {
+    console.log(question);
+    return (
+        <div className="question-wrapper">
+            <LabelButton content={`질문 ${question.question_order}`} />
+            <p className="question-text">{question.question_content}</p>
+
+            <div className="option-container">
+                <OptionRenderer question={question} answers={answers} onChange={onChange} />
+            </div>
+        </div>
+    );
+}
