a7098836f7b2469112bca73941dc163708b2b972
unanseol <d2310@e-mirim.hs.kr>
2025-12-29 14:17:04 +0900

    시민제안 작성 페이지 수정

--- frontend/src/pages/OnboardingPage.js
+++ frontend/src/pages/OnboardingPage.js
@@ -46,8 +46,8 @@ export default function OnboardingPage() {
                 <img src={nuri} alt="" />
                 <Choice userId={sessionStorage.getItem("user_id")} />
                 <div className="onboarding-content" style={{ gap: "5rem", flexDirection: "row" }}>
-                    <TextButtonS content="누리담터 둘러보기" onClick={() => window.location.href = "/nurisodam"} />
-                    <TextButtonS content="제안 시작하기" type="action" onClick={() => window.location.href = "/writeproposal"} />
+                    <TextButtonS content="누리담터 둘러보기" onClick={() => (window.location.href = "/nurisodam")} />
+                    <TextButtonS content="제안 시작하기" type="action" onClick={() => (window.location.href = "/proposal")} />
                 </div>
             </div>
         </div>
--- frontend/src/pages/WriteProposalPage.js
+++ frontend/src/pages/WriteProposalPage.js
@@ -1,14 +1,33 @@
 import Header from "../components/Header";
 import banner from "../assets/image/proposal/proposalbanner.svg";
-import ProfileCard from "../components/home/ProfileCard";
+import DropdownBox from "../ui/input/FormDropdown";
+import TextButtonS from "../ui/button/TextButtonS";
 
 export default function WriteProposalPage() {
+    const optionData = [
+        { key: 1, value: "복지·교육" },
+        { key: 2, value: "교통·환경" },
+        { key: 3, value: "행정·민원" },
+        { key: 4, value: "안전·재난" },
+        { key: 5, value: "경제·상권" },
+        { key: 6, value: "문화·관광" },
+        { key: 7, value: "청년·일자리" },
+    ];
+
     return (
         <div className="writeproposal-container">
             <Header />
             <img src={banner} width="100%" alt="" style={{ marginTop: "6.25rem" }} />
             <div className="writeproposal-wrapper">
-                <DropdownBox />
+                <DropdownBox optionData={optionData} placeholder="카테고리" />
+                <div className="writeproposal-contant">
+                    <input className="title-input" style={{ width: "95rem" }} placeholder="제목을 입력해주세요." />
+                    <textarea className="content-input" style={{ width: "95rem", height: "37.5rem" }} placeholder="내용을 입력해주세요." />
+                </div>
+                <div className="writeproposal-button">
+                    <TextButtonS content="사진첨부" />
+                    <TextButtonS content="제안하기" onClick={() => (window.location.href = "/proposal")} />
+                </div>
             </div>
         </div>
     );
