a3740a017c84c375c1fdf010dbf61043e748716c
gheun <gheun098@gmail.com>
2026-01-05 00:56:35 +0900

    검색창 검색 로직 파라미터 추가

--- frontend/src/ui/input/SearchBar.js
+++ frontend/src/ui/input/SearchBar.js
@@ -1,16 +1,16 @@
 import SearchIcon from "../icons/SearchIcon";
 import SearchDropdown from "../input/SearchDropdown";
 
-export default function SearchBar({ type = "short", value, onChange, onSearch, onCategoryChange, onClick }) {
-    const styles = {
-        width: type === "long" ? "95rem" : "78.8125rem",
-    };
+export default function SearchBar({ type = "short", value, onChange, onSearch, onCategoryChange }) {
+    const styles = { width: type === "long" ? "95rem" : "78.8125rem" };
+
     return (
-        <div className="search-container" style={styles} onClick={onClick}>
+        <div className="search-container" style={styles}>
             <SearchDropdown onChange={onCategoryChange} />
-            <div className="search-content" style={styles} onClick={onClick}>
+
+            <div className="search-content" style={styles}>
                 <input className="search-input" placeholder="검색어를 입력해주세요" value={value} onChange={(e) => onChange(e.target.value)} onKeyDown={(e) => { if (e.key === "Enter") onSearch(); }} />
-                <SearchIcon size={44} style={{ cursor: "pointer" }} />
+                <SearchIcon size={44} style={{ cursor: "pointer" }} onClick={onSearch} />
             </div>
         </div>
     );
